3b972e3b10996197eb223adb4be710d86e92b826
aamaxaa <aamaxaa@users.noreply.github.com>
2024-01-24 22:52:30 -0800

    Created the basic layout of the card match game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [size, setSize] = useState("12");
+  const [start, setStart] = useState(false);
+  function handleStart() {
+    setStart(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Card Match</h1>
+      {start ? <Timer /> : null}
+      <SelectBoard size={size} onSelect={setSize} onStart={handleStart} />
+      <Board size={size} />
     </div>
   );
+}
+function SelectBoard({ size, onSelect, onStart }) {
+  return (
+    <div className="inputs">
+      <label>Board Size:</label>
+      <select value={size} onChange={(e) => onSelect(Number(e.target.value))}>
+        <option value="12">4x3</option>
+        <option value="16">4x4</option>
+        <option value="20">4x5</option>
+      </select>
+      <button onClick={onStart}>Start</button>
+    </div>
+  );
+}
+function Timer() {
+  return <div className="inputs">00:00</div>;
+}
+function Board({ size }) {
+  let nums = Array.from({ length: size }, (_, i) => i + 1);
+  return (
+    <div className="board">
+      {nums.map((num) => (
+        <Card key={num}></Card>
+      ))}
+    </div>
+  );
+}
+function Card() {
+  return <div className="cards"></div>;
 }
 
 export default App;
